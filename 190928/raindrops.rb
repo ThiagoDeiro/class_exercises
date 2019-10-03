661d3ebda16b597874c307a4c17396861c0f2df6
ThiagoDeiro <thiagodeiro.11@hotmail.com>
2019-10-03 20:14:00 +1000

    test

---
+++ 190928/raindrops.rb
@@ -0,0 +1,32 @@
+a = ["Anil", "Erik", "Jonathan"]
+
+p a[1]
+
+a.push("Thiago")
+
+p a
+
+
+# read ruby documentation!
+
+# another coment
+
+
+tv_shows = [
+    ['seinfield', 'prime'],
+    ['satrnger things', 'Netflix'],
+    ['cobra kai', 'youtube']
+]
+
+p tv_shows[2][1]
+
+
+numbers = [1, 2, 3, 4, 5]
+
+power_of_two = numbers.map{ |number| number **2}.each{ |number| p number}
+
+
+#  using map to change a exiting array, if just interating use .each do 
+
+# power_of_two2 = []
+# number.each do |number|s
---
+++ 191002/oscar_exercise.rb
@@ -0,0 +1,44 @@
+@latest_oscar_winners = [
+    'Green Book (2018)',
+    'The Shape of Water (2017)',
+    '12 Years a Slave (2013)',
+    'Moonlight (2016)',
+    'Birdman or (The Unexpected Virtue of Ignorance) (2014)',
+    'Spotlight (2015)',
+    'Argo (2012)',
+    'The Artist (2011)'
+]
+
+@movie_directors = [
+    'Moonlight, Barry Jenkins',
+    'Green Book, Peter Farrelly',
+    'The Artist, Michel Hazanavicius',
+    'Spotlight, Tom McCarthy',
+    'Birdman or (The Unexpected Virtue of Ignorance), Alejandro G. Iñárritu',
+    'The Shape of Water, Guillermo del Toro',
+    '12 Years a Slave, Steve McQueen',
+    'Argo, Ben Affleck',
+]
+
+
+@movie_directors *= ','
+@movie_directors = @movie_directors.split(",")
+ @movie_directors = @movie_directors.values_at(*@movie_directors.each_index.select { |i| i.odd?})
+@new_array = []
+def winners
+    @latest_oscar_winners.zip(@movie_directors).each do |movies, director|
+    oscar_split = movies.split(/[^[[:word:]]]+/)
+    oscar_join = [movies.scan(/\d/).join]
+    oscar_3 = oscar_split - oscar_join
+
+    
+    hash = { :movie => oscar_3 * " ", :year => (oscar_join * " ").to_i, :movie_directors => director}
+    @new_array.push(hash)
+end
+end
+
+winners
+
+p @new_array
+
+# this is not a commment anymore
