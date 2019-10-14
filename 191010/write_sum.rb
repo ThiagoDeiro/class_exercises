f14c59a29f22eb33754f777a2c113ac54dfed538
ThiagoDeiro <thiagodeiro.11@hotmail.com>
2019-10-14 19:08:56 +1100

    saturday work

--- 191010/write_sum.rb
+++ 191010/write_sum.rb
@@ -3,8 +3,18 @@
 
 # s1 = 1 - 1 + 1 - 1 + 1 - 1 + //etc
 # s2 = 1 + 2 + 3 + 4 + 5 + 6 + //etc
-# For example:
 
 # s1(4) = 0
 # s2(4) = 10
-# Can you guess what the sum would be if n is infinity?
+# Can you guess what the sum would  be if n is infinity?
+
+def s1 num
+	num % 2
+end
+​
+def s2 num
+	(0..num).sum
+end
+​
+p s1(4)
+p s2(4)
